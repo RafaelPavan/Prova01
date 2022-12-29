45937fc485015eeaafde5925e2a8bf33cfdb4fa9
Rafael Pavan <rafael.202010386@unilasalle.edu.br>
2022-12-29 11:15:40 -0300

    lógica de descobrir valor total das vendas de um vendedor

--- src/main/java/org/agiBank/fileHandler/Reading.java
+++ src/main/java/org/agiBank/fileHandler/Reading.java
@@ -1,43 +1,89 @@
 package org.agiBank.fileHandler;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class Reading {
     public static void main(String[] args) throws IOException {
         Path pathToFile = Paths.get("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
                 "\\out\\text.txt");
-        Files.createDirectories(pathToFile.getParent());
-        Files.createFile(pathToFile);
-
-        Scanner scanner = new Scanner(new File("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
-                "\\in\\text.txt"));
-        PrintStream ps = new PrintStream(pathToFile.toFile());
-
-        try{
-            while (scanner.hasNextLine()){
-                String line = scanner.nextLine();
-                String [] values = line.split("ç");
-                String function = values[0];
-                String nome = values[2];
-                HashSet<String> nomes = new HashSet<>();
-
-                if(function.startsWith("001")){
-                    ps.println(nome);
-                }
-                if (function.startsWith("002")){
-
-                    ps.println(nomes.add(nome));
-                }
-            }
-        }catch (Exception e){
+        try {
+            Files.createDirectories(pathToFile.getParent());
+            Files.createFile(pathToFile);
+        } catch (FileAlreadyExistsException e) {
             System.out.println(e.getMessage());
         }
+
+        salesmen();
+        customers();
+    }
+
+    public static String salesmen() throws IOException {
+        Scanner scanner = new Scanner(new File("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\in\\text.txt"));
+        PrintStream ps = new PrintStream("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\out\\text.txt");
+        HashSet<String> nomes = new HashSet<>();
+
+        try {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] values = line.split("ç");
+                String function = values[0];
+                List<String> salesmen = Collections.singletonList(values[2]);
+
+                if (function.startsWith("001")) {
+                    nomes.addAll(salesmen);
+                }
+            }
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+        ps.print("Quantidade de vendedores: ");
+        ps.println(nomes.size());
         scanner.close();
         ps.close();
+
+        return nomes.toString();
+    }
+
+    public static String customers() throws IOException {
+        Scanner scanner = new Scanner(new File("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\in\\text.txt"));
+        PrintStream ps = new PrintStream("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\out\\text.txt");
+        HashSet<String> nomes = new HashSet<>();
+
+        try {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] values = line.split("ç");
+                String function = values[0];
+                List<String> customers = Collections.singletonList(values[2]);
+
+                if (function.startsWith("002")) {
+                    nomes.addAll(customers);
+                }
+            }
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+        ps.print("Quantidade de clientes: ");
+        ps.println(nomes.size());
+        scanner.close();
+        ps.close();
+
+        return nomes.toString();
     }
 }
