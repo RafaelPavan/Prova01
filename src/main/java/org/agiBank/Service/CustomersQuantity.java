eb8a37bb4a1a3f72031c45fc534abd98bd47b7b1
Rafael Pavan <rafael.202010386@unilasalle.edu.br>
2023-01-05 14:37:22 -0300

    ajustes finalizados

---
+++ src/main/java/org/agiBank/Service/CustomersQuantity.java
@@ -0,0 +1,66 @@
+package org.agiBank.Service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomersQuantity {
+    public static void main(String[] args) throws IOException {
+        customers();
+    }
+
+    public static void customers() throws IOException {
+        Scanner scanner = new Scanner(new File("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\in\\text.txt"), StandardCharsets.UTF_8);
+        FileWriter fileWriter = new FileWriter("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\out\\textOut.txt", true);
+        FileWriter fileError = new FileWriter("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org" +
+                "\\agiBank\\messages\\messageText.txt", true);
+        fileError.write("\n"+"\nQuantidade de Clientes ");
+
+
+        List<String> generalcustomer = new ArrayList<>();
+        List<String> customerQuantity = new ArrayList<>();
+
+        try {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith("002")) {
+                    generalcustomer.add(List.of(line).toString());
+                }else{
+                    fileError.write("\nId: " + line.substring(0, 3) + " não pertence a pesquisa " +
+                            "de quantidade de clientes.");
+                }
+            }
+
+            String regex = "[A-Z].*[À-ú]*ç*(?=ç)";
+            Pattern pattern = Pattern.compile(regex);
+            for (int i = 0; i < generalcustomer.size(); i++) {
+                Matcher matcher = pattern.matcher(generalcustomer.get(i));
+                while (matcher.find()) {
+                    ArrayList<String> regexList = new ArrayList<>(Collections.singleton(matcher.group()));
+                    customerQuantity.addAll(regexList);
+                }
+            }
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+
+
+        fileWriter.write("\nQuantidade de Clientes: " + customerQuantity.size());
+
+        scanner.close();
+        fileWriter.close();
+        fileError.close();
+    }
+}
---
+++ src/main/java/org/agiBank/Service/SalesmenQuantity.java
@@ -0,0 +1,63 @@
+package org.agiBank.Service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SalesmenQuantity {
+    public static void main(String[] args) throws IOException {
+        salesmen();
+    }
+
+    public static void salesmen() throws IOException {
+        Scanner scanner = new Scanner(new File("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\in\\text.txt"), StandardCharsets.UTF_8);
+        FileWriter ps = new FileWriter("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org\\agiBank\\data" +
+                "\\out\\textOut.txt", true);
+        FileWriter fileError = new FileWriter("C:\\JAVA\\ProvaAgibank\\src\\main\\java\\org" +
+                "\\agiBank\\messages\\messageText.txt", StandardCharsets.UTF_8,true);
+        fileError.write("\n"+"\nServiço de quantidade de Vendedores ");
+
+        List<String> generalSalesman = new ArrayList<>();
+        List<String> salesmanQuantity = new ArrayList<>();
+
+        try {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (line.startsWith("001")) {
+                    generalSalesman.add(List.of(line).toString());
+                }else{
+                    fileError.write("\nId: " + line.substring(0, 3) + " não pertence a pesquisa " +
+                            "de quantidade de vendedores.");
+                }
+            }
+
+            String regex = "[A-Z].*[À-ú]*ç*(?=ç)";
+            Pattern pattern = Pattern.compile(regex);
+            for (int i = 0; i < generalSalesman.size(); i++) {
+                Matcher matcher = pattern.matcher(generalSalesman.get(i));
+                while (matcher.find()) {
+                    ArrayList<String> regexList = new ArrayList<>(Collections.singleton(matcher.group()));
+                    salesmanQuantity.addAll(regexList);
+                }
+            }
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+
+        ps.write("\nQuantidade de Vendedores: " + salesmanQuantity.size());
+
+        scanner.close();
+        ps.close();
+        fileError.close();
+    }
+
+}
